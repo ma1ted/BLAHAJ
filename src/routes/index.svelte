a3b63c8de75526a0fb80544b6a7fafe9d71e9114
malted <malted@malted.dev>
2022-06-02 12:38:47 +0100

    :mag: FÖRBÄTTRA METATAGGAR

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -20,17 +20,17 @@
 
     <!-- Open Graph / Facebook -->
     <meta property="og:type" content="website">
-    <meta property="og:url" content="https://metatags.io/">
+    <meta property="og:url" content="https://blahaj.malted.dev/">
     <meta property="og:title" content="BLÅHAJ">
     <meta property="og:description" content="Alla älskar den här lilla blåhajen. Det ger de bästa kramarna. Det spelar ingen roll om du gråter in i det. Det är det bästa.">
-    <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png">
+    <meta property="og:image" content="https://blahaj.malted.dev/blahaj/blahaj-0.png">
 
     <!-- Twitter -->
     <meta property="twitter:card" content="summary_large_image">
-    <meta property="twitter:url" content="https://metatags.io/">
+    <meta property="twitter:url" content="https://blahaj.malted.dev/">
     <meta property="twitter:title" content="BLÅHAJ">
     <meta property="twitter:description" content="Alla älskar den här lilla blåhajen. Det ger de bästa kramarna. Det spelar ingen roll om du gråter in i det. Det är det bästa.">
-    <meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png">
+    <meta property="twitter:image" content="https://blahaj.malted.dev/blahaj/blahaj-0.png">
 
 </svelte:head>
 
